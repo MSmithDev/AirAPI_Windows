888f28f18c034a5f02805131e8f73c26e09e75f1
Matt <43940868+MSmithDev@users.noreply.github.com>
2023-03-22 16:28:43 -0400

    Added functions change calibration

--- AirAPI_Windows.h
+++ AirAPI_Windows.h
@@ -20,3 +20,15 @@ extern "C" AIR_API float* GetEuler();
 
 //Function to get brightness
 extern "C" AIR_API int GetBrightness();
+
+//Function to set fusion gain
+extern "C" AIR_API int SetFusionGain(float);
+
+//Function to set accel rejection
+extern "C" AIR_API int SetFusionAccelRejection(float);
+
+//Function to set mag rejection 
+extern "C" AIR_API int SetFusionMagRejection(float);
+
+//Function to set mag rejection 
+extern "C" AIR_API int SetFusionRejectTimeout(unsigned int);
